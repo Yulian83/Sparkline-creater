bd8c626539ae4b3d1172e4c3ba0105fcbb94b046
Maksimov Yulian <maksimovyou@gmail.com>
2024-01-18 13:12:41 +0600

    modified:   .gitignore 	modified:   test.py

--- test.py
+++ test.py
@@ -6,49 +6,42 @@ import mplcyberpunk
 from scipy.interpolate import make_interp_spline
 
 
-class Graph:
-    title: str
-
-    def __init__(self) -> None:
-        pass
-
-
-
-
-
-
-
-
-
-class Sparkline(Graph):
+class Sparkline:
     def __init__(self, y_coord: list, title: str = None) -> None:
-        self.y_coord = np.array(y_coord)
-        self.x_coord = np.array([i for i in range(len(self.y_coord))])
-        self.title = title
+        self.__y_coord = np.array(y_coord)
+        self.__x_coord = np.array([i for i in range(len(self.__y_coord))])
+        self.__title = title
+
 
 
     
-    def create(self):
-        with plt.style.context('cyberpunk'):
-            x_dots = self.x_coord
-            y_dots = self.y_coord         
+    def create(self) -> int:
+        try:
+            with plt.style.context('cyberpunk'):
+                x_dots = self.__x_coord
+                y_dots = self.__y_coord         
 
-            x_lines = np.linspace (x_dots.min(), x_dots.max(), 100)
-            spl = make_interp_spline(x_dots, y_dots, k = 2)
-            y_lines = spl(x_lines)     
+                x_lines = np.linspace (x_dots.min(), x_dots.max(), 100)
+                spl = make_interp_spline(x_dots, y_dots, k = 3)
+                y_lines = spl(x_lines)     
 
-            fig = plt.figure()
-            ax = fig.add_subplot()    
+                fig = plt.figure()
+                ax = fig.add_subplot()    
 
-            ax.plot(x_lines, y_lines)
-            ax.set_axis_off()
-            mplcyberpunk.add_glow_effects(gradient_fill=True)
-            plt.savefig('foo.png')
-            plt.show()
+                if(self.__title != None):
+                    ax.set_title(self.__title)
+                ax.plot(x_lines, y_lines)
+                ax.set_axis_off()
+                mplcyberpunk.add_glow_effects(gradient_fill=True)
+
+                plt.savefig('./data/foo.png')
+            return 1
+        except:
+            return 0
     
         
         
 
-sl = Sparkline(y_coord=[3, 0, 5, 0, 7, 3, 2])
+sl = Sparkline(y_coord=[1, 0, 1, 0, 1], title="TITLE")
 
-sl.create()
+result = sl.create()
